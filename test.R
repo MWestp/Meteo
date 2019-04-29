bd5cb0021906087ee3c5c719813569e3d226e7ce
MWestp <49684979+MWestp@users.noreply.github.com>
2019-04-29 19:08:17 +0200

    Add files via upload

---
+++ test.R
@@ -0,0 +1,12 @@
+Test Script
+if
+then
+dies
+das
+Änderung
+
+
+git remote add origin https://github.com/MWestp/Meteo.git
+git config remote.origin.url git@github.com:MWestp/Meteo.git
+git pull -u origin master
+git push -u origin master
